4c442a4de0c741de486901372f0197ca97b645da
vosvosvos <indvfx@gmail.com>
2022-10-14 18:13:39 +0200

    Add packages

---
+++ WachtWoord/SQLite/WachtContext.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.Data.Sqlite;
+using Microsoft.EntityFrameworkCore;
+using WachtWoord.Models;
+
+namespace WachtWoord.SQLite
+{
+    internal class WachtContext : DbContext
+    {
+        public const string DEFAULTDBFILE = "blogging.db";
+
+        public DbSet<Entry> Entries { get; set; }
+
+        private readonly string dbFile = DEFAULTDBFILE;
+        private SqliteConnection connection;
+
+        public WachtContext() { }
+
+        public WachtContext(string databaseFile)
+        {
+            if (!string.IsNullOrEmpty(databaseFile)) dbFile = databaseFile;
+        }
+
+        public WachtContext(SqliteConnection sqliteConnection)
+        {
+            if (!string.IsNullOrEmpty(sqliteConnection?.DataSource)) dbFile = sqliteConnection.DataSource;
+            connection = sqliteConnection;
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            connection ??= InitializeSQLiteConnection(dbFile);
+            optionsBuilder.UseSqlite(connection);
+        }
+
+        private static SqliteConnection InitializeSQLiteConnection(string databaseFile)
+        {
+            var connectionString = new SqliteConnectionStringBuilder
+            {
+                DataSource = databaseFile,
+                Password = "Test123"// PRAGMA key is being sent from EF Core directly after opening the connection
+            };
+            return new SqliteConnection(connectionString.ToString());
+        }
+    }
+}
