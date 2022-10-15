7da857ff6779f42b8861b4e4b556c12c7d6da994
vosvosvos <indvfx@gmail.com>
2022-10-15 23:20:42 +0200

    Created table with striped rows for homepage
    This table will eventually show all entries and the ability to edit them, etc.

---
+++ WachtWoord/Models/EntryService.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WachtWoord.Models
+{
+    internal class EntryService : IEntryService
+    {
+        public void AddEntry(Entry entry)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteEntry(Entry entry)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Entry> GetEntries()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateEntry(Entry entry)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ WachtWoord/Models/IEntryService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WachtWoord.Models
+{
+    internal interface IEntryService
+    {
+        public void AddEntry(Entry entry);
+        public void DeleteEntry(Entry entry);
+        public void UpdateEntry(Entry entry);
+        public List<Entry> GetEntries();
+        
+    }
+}
