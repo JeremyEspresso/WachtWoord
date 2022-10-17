781e3c70d345bd748b220f71a5acc85afd2ff9ec
vosvosvos <indvfx@gmail.com>
2022-10-17 07:02:27 +0200

    Add buttons to table for now

--- WachtWoord/Models/Entry.cs
+++ WachtWoord/Models/Entry.cs
@@ -3,6 +3,8 @@
     public class Entry
     {
         public int Id { get; set; }
+        public string Title { get; set; }
+        public string URL { get; set; } 
         public string Username { get; set; }
         public string Password { get; set; }
     }
