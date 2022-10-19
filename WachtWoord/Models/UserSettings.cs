069c0c6760f15513e39a93cf62cf9364b5441886
vosvosvos <indvfx@gmail.com>
2022-10-19 08:34:45 +0200

    Create UserSettings.cs

---
+++ WachtWoord/Models/UserSettings.cs
@@ -0,0 +1,21 @@
+﻿/*
+ * <summary>
+ * This class is used for the settings.json file
+ * It contains the default settings for the application
+ * </summary>
+ */
+
+namespace WachtWoord.Models
+{
+    public class UserSettings
+    {
+        public string lower = "abcdefghijklmnopqrstuvwxyz";
+        public string upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        public string numbers = "0123456789";
+        public string specials = "!@#$%^&*()_+-=[]{}|;':,./<>?`~";
+        public bool useUpper = true;
+        public bool useLower = true;
+        public bool useNumbers = true;
+        public bool useSpecials = true;
+    }
+}
