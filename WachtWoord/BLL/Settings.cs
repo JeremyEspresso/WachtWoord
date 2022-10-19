39f5f4c05b24b2eb2ba0ebd2d7c2e466c67e05df
vosvosvos <indvfx@gmail.com>
2022-10-19 10:26:15 +0200

    Update Settings.cs

--- WachtWoord/BLL/Settings.cs
+++ WachtWoord/BLL/Settings.cs
@@ -1,19 +1,39 @@
 ﻿using Newtonsoft.Json;
-using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 using WachtWoord.Models;
 
 namespace WachtWoord.BLL
 {
-    public class EditUserSettings
+    public class Settings
     {
-        public static void UpdateSettings(UserSettings userSettings) 
+        //public static readonly string path = Environment.CurrentDirectory + "settings.json";
+        public static readonly string path = "C:\\Users\\duane\\source\\repos\\WachtWoord\\WachtWoord\\settings.json";
+        public void Create()
+        {
+            if (File.Exists(path)) return;
+            using (FileStream fs = File.Create(path))
+            {
+                string JSONres = JsonConvert.SerializeObject(new UserSettings(), Formatting.Indented);
+                byte[] info = new UTF8Encoding(true).GetBytes(JSONres);
+                fs.Write(info, 0, info.Length);
+            };
+        }
+        public void Update(UserSettings userSettings)
         {
             string JSONres = JsonConvert.SerializeObject(userSettings, Formatting.Indented);
-            File.WriteAllText(JSONCreate.path, JSONres);
+            File.WriteAllText(path, JSONres);
+        }
+
+        public void Delete()
+        {
+
+            File.Delete(path);
+        }
+
+        public UserSettings Read()
+        {
+            string JSONres = File.ReadAllText(path);
+            return JsonConvert.DeserializeObject<UserSettings>(JSONres);
         }
         
     }
