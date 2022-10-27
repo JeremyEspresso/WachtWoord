adb6af491aa984de7e228cfadc5931ed988a4d02
vosvosvos <indvfx@gmail.com>
2022-10-27 16:19:55 +0200

    Added nullable values

--- WachtWoord/Models/Entry.cs
+++ WachtWoord/Models/Entry.cs
@@ -6,10 +6,12 @@ namespace WachtWoord.Models
     {
         [Key]
         public int Id { get; set; }
-        public string Title { get; set; }
-        public string URL { get; set; } 
-        public string Username { get; set; }
+        public string? Title { get; set; }
+        public string? URL { get; set; } 
+        public string? Username { get; set; }
+        [Required]
         public string Password { get; set; }
         public int Strength { get; set; }
+        public DateTime CreationDate { get; set; }
     }
 }
