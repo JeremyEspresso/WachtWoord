2ca7d3cda5daca7bce3acd82893bac9298f695f3
vosvosvos <indvfx@gmail.com>
2022-10-28 23:17:03 +0200

    Refactor form

--- WachtWoord/Models/Entry.cs
+++ WachtWoord/Models/Entry.cs
@@ -1,4 +1,5 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace WachtWoord.Models
 {
@@ -6,12 +7,20 @@ namespace WachtWoord.Models
     {
         [Key]
         public int Id { get; set; }
-        public string? Title { get; set; }
-        public string? URL { get; set; } 
+        [Required(ErrorMessage = "Please enter a title")]
+        public string Title { get; set; }
+        public string? URL { get; set; }
         public string? Username { get; set; }
         [Required]
         public string Password { get; set; }
         public int Strength { get; set; }
+        [Required]
+        [NotMapped]
+        [Range(16, 128, 
+            ErrorMessage = "Password length must be between 16 and 128 characters")]
+        [RegularExpression(@"^[0-9]+$", 
+            ErrorMessage = "Length can only contain numbers")]
+        public int Length { get; set; }
         public DateTime CreationDate { get; set; }
     }
 }
