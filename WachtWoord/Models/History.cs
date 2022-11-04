8df7a193f7f86da0400336acd15c47d17a72e447
vosvosvos <indvfx@gmail.com>
2022-11-04 05:58:46 +0100

    History

---
+++ WachtWoord/Models/History.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WachtWoord.Models
+{
+    public class History
+    {
+        public int Id { get; set; }
+        public string Password { get; set; }
+        public DateTime DateChanged { get; set; }
+        public Entry entry { get; set; }
+    }
+}
---
+++ WachtWoord/Models/Services/HistoryService.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WachtWoord.Models.Interfaces;
+using WachtWoord.SQLite;
+
+namespace WachtWoord.Models.Services
+{
+    public class HistoryService : IHistoryService
+    {
+        private readonly Database _db = new();
+        public async void AddHistory(History history)
+        {
+            await _db.History.AddAsync(history);
+            await _db.SaveChangesAsync();
+        }
+
+        public async void DeleteHistory(History history)
+        {
+            if (!await _db.History.ContainsAsync(history)) return;
+            _db.History.Remove(history);
+            await _db.SaveChangesAsync();
+        }
+        
+        public async Task<History?> GetHistory(int id)
+        {
+            return await _db.History.FindAsync(id);
+        }
+
+        public async Task<List<History>> GetHistories()
+        {
+            return await _db.History.ToListAsync();
+        }
+
+        public async Task<List<History>> GetEntryHistory(int entryId)
+        {
+            return await _db.History.Where(h => h.entry.Id == entryId).ToListAsync();
+        }
+
+    }
+}
