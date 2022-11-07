cb38dc7644d9045c8fe4f4051339c987bafbf8bc
vosvosvos <indvfx@gmail.com>
2022-11-07 22:19:01 +0100

    Update History.cs

--- WachtWoord/Models/History.cs
+++ WachtWoord/Models/History.cs
@@ -5,6 +5,5 @@
         public int Id { get; set; }
         public string Password { get; set; }
         public DateTime DateChanged { get; set; }
-        public Entry entry { get; set; }
     }
 }
