b6c2433804c9e3a18efcc0270c4393152f5e5329
Jer <58816840+JeremyEspresso@users.noreply.github.com>
2022-11-10 19:35:44 +0100

    Add MAUI project port

---
+++ WachtWoord.UI/MainPage.xaml.cs
@@ -0,0 +1,17 @@
+﻿namespace WachtWoord;
+
+public partial class MainPage : ContentPage
+{
+	public MainPage()
+    {
+        InitializeComponent();
+
+    }
+
+    protected override void OnAppearing()
+    {
+        base.OnAppearing();
+        this.Window.Height = 728;
+        this.Window.Width = 1024;
+    }
+}
---
+++ WachtWoord.UI/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace WachtWoord;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
